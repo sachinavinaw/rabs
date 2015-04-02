9108667cea4709956b204c86c631859f8f3cc841
sachinavinaw <sachinavinaw@gmail.com>
2015-04-02 12:11:36 +0530

    Changed on local machine

--- git2.java
+++ git2.java
@@ -1,5 +1,6 @@
+//Sample Program
 class git1{
-public static void main()
+public static void main(String args[])
 {
 	System.out.println("Hello RABS Pro.");
 	System.out.println("http://www.rabspro.in");
