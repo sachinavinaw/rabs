08c1cbd6a62c0ea51a6ed7ec845a7036f7746cc3
sachinavinaw <sachinavinaw@gmail.com>
2015-04-02 12:03:36 +0530

    Modifiedd in Git1.java

--- git1.java
+++ git1.java
@@ -1,8 +1,9 @@
 class git1{
-public static void main()
+public static void main(String args[])
 {
 	System.out.println("Hello RABS Pro.");
-	//Semicolon was missing before. 
+	//Semicolon was missing before.
+	//Modified. 
 }
 
 }
